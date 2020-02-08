cc9e80508f8a722f982d6f339c2e32e37c1b54d4
Taka Kodani <tklkdn@gmail.com>
2020-02-08 15:29:19 -0600

    table page

--- movies.js
+++ movies.js
@@ -1,23 +1,22 @@
 var express = require('express');
-var mysql = require('./dbcon.js');
-// var bodyParser = require('body-parser');
-// var path = require('path');
+//var mysql = require('./dbcon.js');
 
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-//app.set('port', 3006);
 
-// app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/',function(req,res,next){
   var context = {};
   res.render('home', context);
+});
+
+app.get('/table',function(req,res,next){
+  var context = {};
+  res.render('table', context);
 });
 
 app.use(function(req,res){
@@ -29,8 +28,4 @@ app.use(function(err, req, res, next){
   console.error(err.stack);
   res.status(500);
   res.render('500');
-});
-
-app.listen(app.get('port'), function(){
-  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
